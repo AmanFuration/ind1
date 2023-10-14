658c9e3ab8a842114a9c3a101454246a8a2b1416
AmanFuration <aman.maddhesiaaman@gmail.com>
2023-10-14 15:08:28 +0530

    change 3

--- newFile1.js
+++ newFile1.js
@@ -1,2 +1,3 @@
 console.log("Hello world by Aman furation with personal key");
 console.log("Hello world by Amanskn  with personal key");
+console.log("Hello world by Amafuration  with personal key");
