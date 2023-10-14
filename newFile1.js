f91124908a4f574f6d138a281ee130033c3f9805
AmanFuration <aman.maddhesia@furation.tech>
2023-10-14 15:10:12 +0530

    change 4

--- newFile1.js
+++ newFile1.js
@@ -1,3 +1,4 @@
 console.log("Hello world by Aman furation with personal key");
 console.log("Hello world by Amanskn  with personal key");
 console.log("Hello world by Amafuration  with personal key");
+console.log("Hello world by Amafuration  with personal key final");
